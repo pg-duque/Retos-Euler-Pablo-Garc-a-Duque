07871fd9ec0ed16487ad962c95b9d80398649a1a
pg-duque <pablogardu21@gmail.com>
2025-11-25 14:59:44 +0100

    He comentado el código para hacerlo un poco más claro

--- src/EjercicioRegistro/Main.java
+++ src/EjercicioRegistro/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
         long suma = 0;
 
+        // Para resolver el ejercicio, hacemos un for simple con el que recorrer la lista de números.
+        //Inicializamos en 1 para tener el primer impar, y aumentamos el iterador de 2 en 2, para saltarnos los pares.
         for (long i = 1; i<=901000; i+=2){
+            // Para cada recorrido del for, sumamos lo que tenemos (la variable "suma") con el cuadrado del nuevo número de la lista.
             suma = suma + (i*i);
         }
         System.out.println("Suma: " + suma);
